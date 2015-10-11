29966b3a27b881b4d6e37f9ffd027aac8529765c
Chua Chin Siang <ccs6327@gmail.com>
2015-10-10 17:16:23 -0700

    login enabled

---
+++ sahara_main/client/login.js
@@ -0,0 +1,12 @@
+Template.login.events({
+	'click #buttonLogin': function(event){
+		Meteor.loginWithFacebook({
+			requestPermissions: ['user_friends']
+		}, function(err){
+			if (err){
+				throw new Meteor.Error("Facebook login failed");
+			}
+			Router.go('home');
+		});
+	}
+});
