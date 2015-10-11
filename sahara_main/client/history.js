6b3c34436466b59ebb9434f697e6c0c2a8c9c2f5
kangsoontan <tankangsoon@gmail.com>
2015-10-10 20:35:37 -0700

    History.js

--- sahara_main/client/history.js
+++ sahara_main/client/history.js
@@ -1,7 +1,6 @@
 Template.history.helpers({
 	'plan': function(){
 		sessionSummary = Sessions.find();
-		sessionSummary['datetime'] = datetimeToDisplayFormat(sessionSummary['datetime']);
 		return sessionSummary;
 	}
 });
