1129a7d47d0df66bde11ac6616b6da947115e6ff
Chua Chin Siang <ccs6327@gmail.com>
2015-10-11 22:27:21 -0700

    clipboard copy ios

--- sahara_main/client/session.js
+++ sahara_main/client/session.js
@@ -43,7 +43,8 @@ Template.session.events({
 Template.session.rendered = function() {
 	$('#copyLink').click(function(){
 		copyTextarea = document.querySelector('#copyLink');
-	    copyTextarea.select();
+		copyTextarea.setSelectionRange(0, this.value.length);
+	    // copyTextarea.select();
 		try {
 			var successful = document.execCommand('copy');
 			Session.set("isLinkCopied", successful);
