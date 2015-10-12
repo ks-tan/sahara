19c8bf6ae710236101d408744b222f7a4179812f
Chua Chin Siang <ccs6327@gmail.com>
2015-10-11 21:03:08 -0700

    merge

--- sahara_main/client/login.js
+++ sahara_main/client/login.js
@@ -1,7 +1,8 @@
 Template.login.events({
 	'click #buttonLogin': function(event){
 		Meteor.loginWithFacebook({
-			requestPermissions: ['user_friends']
+			requestPermissions: ['user_friends'],
+			redirectUrl: "http://sahara.meteor.com"
 		}, function(err){
 			if (err){
 				throw new Meteor.Error("Facebook login failed");
